0cba30414438d8deb605ef5b99bb1f6575ae5ee5
Saurabh Tewary <tewary.saurabh@gmail.com>
2020-05-24 18:27:42 +0530

    RA-2 #commit Added management command to wait for DB and also Postgres DB

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,6 +4,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
 
+
 class CommandTests(TestCase):
     """
     Class to test DB Commands
