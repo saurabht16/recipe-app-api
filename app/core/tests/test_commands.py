c27f249943e7ba1168ea5a95baff8af014b13474
Saurabh Tewary <tewary.saurabh@gmail.com>
2020-05-25 18:00:53 +0530

    RA-2 #commit Added management command to wait for DB and also Postgres DB

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -15,18 +15,18 @@ class CommandTests(TestCase):
         Test waiting for db when db is vailable
         :return:
         """
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+        with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
             gi.return_value = True
-            call_command('wait_for_db')
+            call_command("wait_for_db")
             self.assertEqual(gi.call_count, 1)
 
-    @patch('time.sleep', return_value=True)
+    @patch("time.sleep", return_value=True)
     def test_wait_for_db(self, ts):
         """
         Test waiting for DB
         :return:
         """
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+        with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
-            call_command('wait_for_db')
+            call_command("wait_for_db")
             self.assertEqual(gi.call_count, 6)
