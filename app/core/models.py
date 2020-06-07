22032a96ca610f74ed5a83d6dcdfc4525370ab43
Saurabh Tewary <tewary.saurabh@gmail.com>
2020-06-07 12:30:40 +0530

    RA-2 #commit Added Recipe App and tags API
    Signed-off-by: Saurabh Tewary <tewary.saurabh@gmail.com>

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                 PermissionsMixin
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
     """
@@ -51,3 +53,20 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """
+    Tag to be used for recipe
+    """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             )
+
+    def __str__(self):
+        """
+        STR representation of TAG
+        :return: self.name
+        """
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@djangoappdev.com', password='testpass'):
+    """
+    Create a sample user
+    :param email: email of user
+    :param password: password
+    :return: new user
+    """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -50,3 +61,14 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """
+        Test the tag str representation
+        :return:
+        """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
