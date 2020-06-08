80c7df7998f767b3dcd99e45cd12a548004689dd
Saurabh Tewary <tewary.saurabh@gmail.com>
2020-06-08 17:32:01 +0530

    RA-2 #commit Added Create Ingredient feature
    Signed-off-by: Saurabh Tewary <tewary.saurabh@gmail.com>

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -76,3 +76,27 @@ class PrivateIngredientsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """
+        Test create a new ingredient
+        :return:
+        """
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name'],
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """
+        Test create a new invalid ingredient fails
+        :return:
+        """
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
